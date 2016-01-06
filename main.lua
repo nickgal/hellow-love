09749dbd50ea9e2b454e24fe4e0a1233f7331d53
Nick Gal <nickgal@me.com>
2016-01-05 19:09:14 -0800

    add keyboard controls

--- main.lua
+++ main.lua
@@ -1,11 +1,38 @@
 function love.load(arg)
-
+  hellow = { x = 200, y = 200, speed = 150, text = 'Hellow' }
 end
 
 function love.update(dt)
-
+  if love.keyboard.isDown('up') then
+    moveUp(dt)
+  end
+  if love.keyboard.isDown('down') then
+    moveDown(dt)
+  end
+  if love.keyboard.isDown('left') then
+    moveLeft(dt)
+  end
+  if love.keyboard.isDown('right') then
+    moveRight(dt)
+  end
 end
 
 function love.draw()
-  love.graphics.print('Hellow', 200, 200)
+  love.graphics.print(hellow.text, hellow.x, hellow.y)
+end
+
+function moveUp(dt)
+  hellow.y = hellow.y - (hellow.speed * dt)
+end
+
+function moveDown(dt)
+  hellow.y = hellow.y + (hellow.speed * dt)
+end
+
+function moveLeft(dt)
+  hellow.x = hellow.x - (hellow.speed * dt)
+end
+
+function moveRight(dt)
+  hellow.x = hellow.x + (hellow.speed * dt)
 end
